d68556d15fbfd5e5ebd160fa1c77cc9a94bda93c
NinnjA254 <quaypeter@yahoo.com>
2025-01-07 22:12:26 +0300

    done header

---
+++ app/ui/Header.tsx
@@ -0,0 +1,64 @@
+"use client"
+import { SocialIcon } from "react-social-icons"
+import { motion } from "motion/react"
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto xl:items-center p-5" >
+      <motion.div
+        className="flex flex-row items-center"
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1
+        }}
+      >
+        <SocialIcon
+          url="www.github.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="www.twitter.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="www.upwork.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1
+        }}
+      >
+        <SocialIcon
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in touch</p>
+      </motion.div>
+    </header>
+  )
+}
