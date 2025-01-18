92aee6c874578f868c0922e68aea1410e15781a8
PeterALX <quaypeter25@gmail.com>
2025-01-18 12:33:02 +0300

    added a scroll to top/home button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { EnvelopeIcon, HomeModernIcon } from "@heroicons/react/24/solid"
+import Link from "next/link";
 import About from "./ui/About";
 import Contact from "./ui/Contact";
 import Experience from "./ui/Experience";
@@ -8,7 +10,7 @@ import Skills from "./ui/Skills";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#28E98C]/80">
+    <div className="relative bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#28E98C]/80">
       <Header />
       <section id="hero" className="snap-start">
         <Hero />
@@ -28,6 +30,11 @@ export default function Home() {
       <section id="Contact" className="snap-start">
         <Contact />
       </section>
+      <Link href="#hero">
+        <footer className="border border-transparent hover:border-[#28E98C]/70 cursor-pointer fixed bottom-5 right-40 flex items-center justify-center p-2 rounded-full transition-colors">
+          <HomeModernIcon className="h-6 w-6 text-gray-500 hover:text-[#26E96C]/70 transition-colors" />
+        </footer>
+      </Link>
     </div>
   );
 }
